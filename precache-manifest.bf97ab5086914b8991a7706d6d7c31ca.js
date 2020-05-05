d5c5019bed40cf47631c890329eaaeffc087b74c
Patryk Oltuch <oltix@wp.pl>
2020-05-05 13:52:30 +0200

    Updates

---
+++ precache-manifest.bf97ab5086914b8991a7706d6d7c31ca.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d708f8a3f5b72fbc47624d4d62615802",
+    "url": "/syskon_V2/index.html"
+  },
+  {
+    "revision": "ec82cbe02a2afcc58d6d",
+    "url": "/syskon_V2/static/css/main.5c91d9fd.chunk.css"
+  },
+  {
+    "revision": "9e557f7cb29764fa0869",
+    "url": "/syskon_V2/static/js/2.481f50f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/syskon_V2/static/js/2.481f50f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec82cbe02a2afcc58d6d",
+    "url": "/syskon_V2/static/js/main.cd44a51f.chunk.js"
+  },
+  {
+    "revision": "bdb619673c2b59d614e9",
+    "url": "/syskon_V2/static/js/runtime-main.e929a3e8.js"
+  },
+  {
+    "revision": "1f8aab22f54f78b24d3208342777d85d",
+    "url": "/syskon_V2/static/media/dolny.1f8aab22.png"
+  },
+  {
+    "revision": "037b8b30c06e26c5ec322766cd26f46a",
+    "url": "/syskon_V2/static/media/funduszeEuropejskie.037b8b30.jpg"
+  },
+  {
+    "revision": "6b0aa4b73958a35136457379cdb8b095",
+    "url": "/syskon_V2/static/media/mapa.6b0aa4b7.png"
+  },
+  {
+    "revision": "be3a324bf92642780cdf61dcf7dc6081",
+    "url": "/syskon_V2/static/media/syskon1.be3a324b.png"
+  },
+  {
+    "revision": "bd0daf8abae6fb44517550e5b7e9e40b",
+    "url": "/syskon_V2/static/media/uniaEuropejska.bd0daf8a.jpg"
+  }
+]);
